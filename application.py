4861e31eeb8101806bb2d5badd7a3e755c3cdd53
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-09-24 07:11:35 -0600

    Add files via upload

--- application.py
+++ application.py
@@ -7,15 +7,15 @@ Created on Fri Sep 21 06:52:53 2018
 
 import pandas as pd
 
+cum_BCWS, cum_BCWP, cum_ACWP = 0, 0, 0 
+
+def csv_Read():
+    data_file = pd.read_csv('datafile.csv').fillna(0)
+    
 def ev_MetricTypes(data_file):
     acwp = data_file.loc[data_file['Value Type'] == 'ACWP']
     bcwp = data_file.loc[data_file['Value Type'] == 'BCWP']
     bcws = data_file.loc[data_file['Value Type'] == 'BCWS']
-    return acwp, bcwp, bcws
-
-def csv_Read():
-    data_file = pd.read_csv('datafile.csv').fillna(0)
-    return data_file
 
 def cum_Cost():
     cum_CV = cum_BCWP - cum_ACWP
@@ -26,4 +26,9 @@ def cum_Schedule():
     cum_SPI = cum_BCWP / cum_BCWS 
 
 def main():
+    csv_Read()
+    ev_MetricTypes()
+    cum_Cost()
+    cum_Schedule()
     
+main()
