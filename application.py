a85e8461c39de305c0613f7264ebb8e44e1ece0f
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-09-24 07:54:24 -0600

    Add files via upload

--- application.py
+++ application.py
@@ -7,28 +7,30 @@ Created on Fri Sep 21 06:52:53 2018
 
 import pandas as pd
 
+
 cum_BCWS, cum_BCWP, cum_ACWP = 0, 0, 0 
 
 def csv_Read():
-    data_file = pd.read_csv('datafile.csv').fillna(0)
+    csv_DataFrame = pd.read_csv('datafile.csv').fillna(0)
+    print(csv_DataFrame)
     
-def ev_MetricTypes(data_file):
-    acwp = data_file.loc[data_file['Value Type'] == 'ACWP']
-    bcwp = data_file.loc[data_file['Value Type'] == 'BCWP']
-    bcws = data_file.loc[data_file['Value Type'] == 'BCWS']
-
-def cum_Cost():
-    cum_CV = cum_BCWP - cum_ACWP
-    cum_CPI = cum_BCWP / cum_ACWP
-
+def ev_MetricTypes(csv_DataFrame):
+    acwp_DataFrame = csv_DataFrame.loc[csv_DataFrame['Value Type'] == 'ACWP']
+    bcwp_DataFrame = csv_DataFrame.loc[csv_DataFrame['Value Type'] == 'BCWP']
+    bcws_DataFrame = csv_DataFrame.loc[csv_DataFrame['Value Type'] == 'BCWS']
+    
 def cum_Schedule():
     cum_SV = cum_BCWP - cum_BCWS
-    cum_SPI = cum_BCWP / cum_BCWS 
-
+    cum_SPI = cum_BCWP / cum_BCWS
+    
+def cum_Cost(data_file):
+    cum_CV = cum_BCWP - cum_ACWP
+    cum_CPI = cum_BCWP / cum_ACWP
+    
 def main():
     csv_Read()
     ev_MetricTypes()
-    cum_Cost()
     cum_Schedule()
+    cum_Cost()
     
 main()
