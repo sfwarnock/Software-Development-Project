073b4081cb08a85417bbc26f9ede0834063fcf8b
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-09-24 07:08:07 -0600

    Add files via upload

--- Sprints/Sprint 1/cumualtive_cost.py
+++ Sprints/Sprint 1/cumualtive_cost.py
@@ -5,6 +5,8 @@ Created on Mon Sep 24 06:09:29 2018
 @author: Scott Warnock
 """
 
+cum_BCWS, cum_BCWP, cum_ACWP = 0, 0, 0 
+
 def cum_Cost(data_file):
     cum_CV = cum_BCWP - cum_ACWP
     cum_CPI = cum_BCWP / cum_ACWP
