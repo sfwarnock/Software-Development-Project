d08ec926e994675bb5d7caef38ac26124a83d85c
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-09-24 07:07:45 -0600

    Add files via upload

--- Sprints/Sprint 1/cumualative_schedule.py
+++ Sprints/Sprint 1/cumualative_schedule.py
@@ -5,6 +5,8 @@ Created on Mon Sep 24 06:09:32 2018
 @author: 175272
 """
 
+cum_BCWS, cum_BCWP, cum_ACWP = 0, 0, 0 
+
 def cum_Schedule():
     cum_SV = cum_BCWP - cum_BCWS
     cum_SPI = cum_BCWP / cum_BCWS  
