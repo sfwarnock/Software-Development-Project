76b296cb689e16ef7d32999d34508feb8044b865
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-09-24 07:47:46 -0600

    Add files via upload

--- Sprints/Sprint 1/csv_read.py
+++ Sprints/Sprint 1/csv_read.py
@@ -7,6 +7,10 @@ Created on Fri Sep 21 06:27:36 2018
 
 import pandas as pd
 
+csv_DataFrame = pd.read_csv('datafile.csv').fillna(0)
+
 def csv_Read():
-    data_file = pd.read_csv('datafile.csv').fillna(0)
-    return data_file
+    csv_DataFrame = pd.read_csv('datafile.csv').fillna(0)
+    print(csv_DataFrame)
+
+csv_Read()
