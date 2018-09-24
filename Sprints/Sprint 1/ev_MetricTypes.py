efc75cb9701e9b74e25bf35f53767a763426911d
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-09-24 07:54:47 -0600

    Add files via upload

--- Sprints/Sprint 1/ev_MetricTypes.py
+++ Sprints/Sprint 1/ev_MetricTypes.py
@@ -5,8 +5,7 @@ Created on Fri Sep 21 06:48:38 2018
 @author: Scott Warnock
 """
 
-def ev_MetricTypes(data_file):
-    acwp = data_file.loc[data_file['Value Type'] == 'ACWP']
-    bcwp = data_file.loc[data_file['Value Type'] == 'BCWP']
-    bcws = data_file.loc[data_file['Value Type'] == 'BCWS']
-    return acwp, bcwp, bcws
+def ev_MetricTypes(csv_DataFrame):
+    acwp_DataFrame = csv_DataFrame.loc[csv_DataFrame['Value Type'] == 'ACWP']
+    bcwp_DataFrame = csv_DataFrame.loc[csv_DataFrame['Value Type'] == 'BCWP']
+    bcws_DataFrame = csv_DataFrame.loc[csv_DataFrame['Value Type'] == 'BCWS']
