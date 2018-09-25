5de1993c8d0d88d6f8289fd80a4e92549a5db811
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-09-25 08:05:31 -0600

    Add files via upload

--- Sprints/Sprint 1/ev_MetricTypes.py
+++ Sprints/Sprint 1/ev_MetricTypes.py
@@ -9,3 +9,9 @@ def ev_MetricTypes(csv_DataFrame):
     acwp_DataFrame = csv_DataFrame.loc[csv_DataFrame['Value Type'] == 'ACWP']
     bcwp_DataFrame = csv_DataFrame.loc[csv_DataFrame['Value Type'] == 'BCWP']
     bcws_DataFrame = csv_DataFrame.loc[csv_DataFrame['Value Type'] == 'BCWS']
+    
+    headerValues = dataFrame.columns.values[6:].tolist()
+    
+    acwp.loc['Period Total Cost', headerValues] = acwp[headerValues].sum()
+    bcwp.loc['Period Total Earned', headerValues] = bcwp[headerValues].sum()
+    bcws.loc['Period Total Planned', headerValues] = bcws[headerValues].sum()
