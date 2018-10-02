7ed7360d6828e00f834ed7e2fce63eb5307323b7
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-10-02 09:53:27 -0600

    Add files via upload

--- application/period_schedule.py
+++ application/period_schedule.py
@@ -7,13 +7,13 @@ Created on Tue Sep 25 06:15:24 2018
 
 period_BCWS, period_BCWP, period_ACWP = 0, 0, 0
 
-def period_Schedule():
+def period_Schedule(period_DataFrame, headerValues):
+    period_BCWP = period_DataFrame.loc['Period Total Planned', headerValues]
+    period_BCWS = period_DataFrame.loc['Period Total Earned', headerValues]
+    
     period_SPI = period_BCWP / period_BCWS
     period_SV = period_BCWP - period_BCWS
     
-    period_BCWP = period_DataFrame.loc['Period Total Planned', headerValues]
-    period_BCWS = period_DataFrame.loc['Period Total Earned', headerValues]
-
     period_DataFrame.loc['Period SV'] = period_SV
     period_DataFrame.loc['Period SPI'] = period_SPI
     return period_SPI, period_SV
