b627d2891a97158341e0239928edf0072098279a
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-10-05 06:41:05 -0600

    Add files via upload

--- application/filter_CAM.py
+++ application/filter_CAM.py
@@ -5,6 +5,8 @@ Created on Mon Oct  1 06:21:41 2018
 @author: Scott Warnock
 """
 
-def filter_CAM():
-    filter_CAM = pd.pivot_table(period_DataFrame, values = csv_header, index=['CAM','Charge Code' 'Value Type'])
-    return filter_CAM
+def filter_CAM(period_DataFrame, cum_DataFrame, csv_DataFrame, csv_header):
+    filter_CAM_csv = pd.pivot_table(csv_DataFrame, values = csv_header, index=['CAM','Charge Code' 'Value Type'])
+    filter_CAM_period = pd.pivot_table(period_DataFrame, values = csv_header, index=['CAM','Charge Code' 'Value Type'])
+    filter_CAM_cum = pd.pivot_table(cum_DataFrame, values = csv_header, index=['CAM','Charge Code' 'Value Type'])
+    return filter_CAM_csv, filter_CAM_period, filter_CAM_cum
--- application/filter_ChargeCode.py
+++ application/filter_ChargeCode.py
@@ -5,6 +5,8 @@ Created on Mon Oct  1 06:21:39 2018
 @author: Scott Warnock
 """
 
-def filter_ChargeCode():
-    filter_ChargeCode = pd.pivot_table(period_DataFrame, values = csv_header, index=['Charge Code', 'CAM', 'Value Type'])
-    return filter_ChargeCode
+def filter_ChargeCode(period_DataFrame, cum_DataFrame, csv_DataFrame, csv_header):
+    filter_ChargeCode_csv = pd.pivot_table(csv_DataFrame, values = csv_header, index=['Charge Code', 'CAM', 'Value Type'])
+    filter_ChargeCode_period = pd.pivot_table(period_DataFrame, values = csv_header, index=['Charge Code', 'CAM', 'Value Type'])
+    filter_ChargeCode_cum = pd.pivot_table(cum_DataFrame, values = csv_header, index=['Charge Code', 'CAM', 'Value Type'])
+    return filter_ChargeCode_period, filter_ChargeCode_cum, filter_ChargeCode_csv
