8ef745c28920f4617ebd03110dbd984b4e82da4d
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-10-09 08:13:16 -0600

    Add files via upload

--- application/data_visualization.py
+++ application/data_visualization.py
@@ -7,26 +7,21 @@ Created on Tue Oct  9 06:14:12 2018
 
 def data_Visualazation(cum_ACWP, cum_BCWP, cum_BCWS, period_ACWP, period_BCWS, period_BCWP, headerValues):
     fig, ax = plt.subplots()
-    ax.plot(cum_ACWP, color = "Blue")
+    ax.plot(cum_ACWP, color = "Green")
     ax.plot(cum_BCWP, color = "Red")
-    ax.plot(cum_BCWS, color = "Green")
+    ax.plot(cum_BCWS, color = "Blue")
 
     ax.set(xlabel='Month', ylabel= '$', title= 'Project S-Curve')
     plt.show()
 
-
-    bar_acwp = period_ACWP
-    bar_bcwp = period_BCWP
-    bar_bcws = period_BCWS
-
-    ind = np.arange(len(bar_bcwp))
+    ind = np.arange(len(period_BCWP))
     width = 0.35
 
     fig, ax = plt.subplots()
 
-    bcwsBar = ax.bar(ind - width/2, bar_bcws, width, color = 'Red', label = 'Planned Value')
-    bcwpBar = ax.bar(ind + width/2, bar_bcwp, width, color = 'Green', label = 'Earned Value')
-    acwpBar = ax.bar(ind + width/2, bar_acwp, width, color = 'Blue', label = 'Actual Cost')
+    bcwsBar = ax.bar(ind - width/2, period_BCWS, width, color = 'Red', label = 'Planned Value')
+    bcwpBar = ax.bar(ind + width/2, period_BCWP, width, color = 'Blue', label = 'Earned Value')
+    acwpBar = ax.bar(ind + width/2, period_ACWP, width, color = 'Green', label = 'Actual Cost')
 
     ax.set_ylabel('$')
     ax.set_xticklabels(headerValues)
