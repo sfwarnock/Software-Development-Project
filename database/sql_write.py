776d2eb12531cf4d5ff84d69f97b1031dbed6936
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-10-19 14:10:58 -0600

    Add files via upload

--- database/sql_write.py
+++ database/sql_write.py
@@ -10,8 +10,8 @@ from sqlalchemy import create_engine
 def sql_database(csv_DataFrame, period_DataFrame, cum_DataFrame):
     engine = create_engine('mssql+pymssql://something:something@hostname:port/dbname')
     
-    csv_DataFrame.to_sql(csv_DataFrame, con = engine, if_exisits = 'replace', index_labels = 'id')
+    csv_DataFrame.to_sql(csv_DataFrame, con = engine, if_exisits = 'append', index_labels = 'id')
     
-    period_DataFrame.to_sql(period_DataFrame, con = engine, if_exisits = 'replace', index_labels = 'id')
+    period_DataFrame.to_sql(period_DataFrame, con = engine, if_exisits = 'append', index_labels = 'id')
     
-    cum_DataFrame.to_sql(cum_DataFrame, con = engine, if_exisits = 'replace', index_labels = 'id')
+    cum_DataFrame.to_sql(cum_DataFrame, con = engine, if_exisits = 'append', index_labels = 'id')
