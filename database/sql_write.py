b8e73cc895f65777b87d4efe8a2744ebd9eb90a7
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-10-27 08:55:28 -0600

    Add files via upload

--- database/sql_write.py
+++ database/sql_write.py
@@ -5,13 +5,16 @@ Created on Tue Oct  9 06:14:12 2018
 @author: Scott Warnock
 """
 
-from sqlalchemy import create_engine
+import pyodbc
 
 def sql_database(csv_DataFrame, period_DataFrame, cum_DataFrame):
-    engine = create_engine('mssql+pymssql://something:something@hostname:port/dbname')
+    conn = pyodbc.connect(host = r'DESKTOP-7HC4GND\SQLEXPRESS',
+                           user = r'name',
+                           password = r'password',
+                           database = r'CU Software Development Project')
     
-    csv_DataFrame.to_sql(csv_DataFrame, con = engine, if_exisits = 'append', index_labels = 'id')
+    csv_DataFrame.to_sql(csv_DataFrame, conn, if_exisits = 'append', index_labels = 'id')
     
-    period_DataFrame.to_sql(period_DataFrame, con = engine, if_exisits = 'append', index_labels = 'id')
+    #period_DataFrame.to_sql(period_DataFrame, con = engine, if_exisits = 'append', index_labels = 'id')
     
-    cum_DataFrame.to_sql(cum_DataFrame, con = engine, if_exisits = 'append', index_labels = 'id')
+    #cum_DataFrame.to_sql(cum_DataFrame, con = engine, if_exisits = 'append', index_labels = 'id')
