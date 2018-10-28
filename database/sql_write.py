6c01f8bce5529dc93d2b323920ba7d6998c56c72
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-10-27 21:02:04 -0600

    Add files via upload

--- database/sql_write.py
+++ database/sql_write.py
@@ -8,10 +8,7 @@ Created on Tue Oct  9 06:14:12 2018
 import pyodbc
 
 def sql_database(csv_DataFrame, period_DataFrame, cum_DataFrame):
-    conn = pyodbc.connect(host = r'DESKTOP-7HC4GND\SQLEXPRESS',
-                           user = r'name',
-                           password = r'password',
-                           database = r'CU Software Development Project')
+    conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER={(LOCAL\SQLEXPRESS};DATABASE={SELECTED DB};Trusted_Connection=yes')
     
     csv_DataFrame.to_sql(csv_DataFrame, conn, if_exisits = 'append', index_labels = 'id')
     
