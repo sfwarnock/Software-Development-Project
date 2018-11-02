d15767f276e8ea46c5d08c50dbf963a1f209eaa0
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-11-02 10:26:10 -0600

    Add files via upload

--- api/dataframe_to_JSON.py
+++ api/dataframe_to_JSON.py
@@ -5,9 +5,11 @@ Created on Fri Oct 19 13:16:11 2018
 @author: Scott Warnock
 """
 
-def dataframe_to_JSON(csv_DataFrame, period_DataFrame, cum_DataFrame):
-    csv_DataFrame_JSON = csv_DataFrame.to_json(orient = 'index')
-    period_DataFrame_json = period_DataFrame.to_json(orient = 'index')
-    cum_DataFrame_json = cum_DataFrame.to_json(orient = 'index')
+def dataframe_to_JSON(cum_DataFrame):
+    periodArray_toJSON = cum_DataFrame.loc[cum_DataFrame.index.isin(['Period Total Planned', 'Period Total Earned', 
+                                                                     'Period Total Cost'])].to_json(orient = 'index')
+
+    cumArray_toJSON = cum_DataFrame.loc[cum_DataFrame.index.isin(['Cumulative Planned Value', 'Cumulative Earned Value', 
+                                                                  'Cumulative Total Cost'])].to_json(orient = 'index')
     
-    return csv_DataFrame_JSON, period_DataFrame_json, cum_DataFrame_json
+    return periodArray_toJSON, cumArray_toJSON
