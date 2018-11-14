a42bd20d33280a727b26e6cdd156127400a068c9
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-11-14 07:00:23 -0700

    Add files via upload

--- user interface/project_data.js
+++ user interface/project_data.js
@@ -4,7 +4,20 @@ var project_todate = $.getJSON( "cum_json.json", function(data) {
 
 acwp = (project_todate)['ACWP'];
 bcwp = (project_todate)['BCWP'];
+bcws = (project_todate)['BCWS'];
 bac = (project_todate)['Budget at Complete'];
+bcwr = (project_todate)['Budget Cost of Work Remaining'];
+cpi = (project_todate)['CPI'];
+cv = (project_todate)['CV'];
+eac = (project_todate)['EAC'];
+percentcom = (project_todate)['Percent Complete'];
+peac = (project_todate)['Performance EAC'];
+petc = (project_todate)['Performance ETC'];
+ptcpi = (project_todate)['Performance TCPI'];
+spi = (project_todate)['SPI'];
+sv = (project_todate)['SV'];
+tcpi = (project_todate)['TCPI'];
+vac = (project_todate)['VAC'];
 
 document.write(acwp);
 document.write(bcwp);
