db5052d30ffa95abb29d107dfb7ed9470f3e3efb
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-11-26 06:08:40 -0800

    Add files via upload

--- user interface/project_data.js
+++ user interface/project_data.js
@@ -1,57 +1,57 @@
 function loadData(project_todate) {
   var acwp = document.getElementById('acwp');
-  acwp.textContent = '$ ' + parseFloat(Math.round(project_todate['ACWP']*100) / 100).toFixed(2);
+  acwp.textContent = '$ ' + parseFloat(project_todate['ACWP']).toLocaleString('en');
 
   var bcwp = document.getElementById('bcwp');
-  bcwp.textContent = '$ ' + parseFloat(Math.round(project_todate['BCWP']*100) / 100).toFixed(2);
+  bcwp.textContent = '$ ' + parseFloat(project_todate['BCWP']).toLocaleString('en');
 
   var bcws = document.getElementById('bcws');
-  bcws.textContent = '$ ' + parseFloat(Math.round(project_todate['BCWS']*100) / 100).toFixed(2);
+  bcws.textContent = '$ ' + parseFloat(project_todate['BCWS']).toLocaleString('en');
 
   var bac = document.getElementById('bac');
-  bac.textContent = '$ ' + parseFloat(Math.round(project_todate['BAC']*100) / 100).toFixed(2);
+  bac.textContent = '$ ' + parseFloat(project_todate['BAC']).toLocaleString('en');
 
   var totalbudget = document.getElementById('totalbudget');
-  totalbudget.textContent = '$ ' + parseFloat(Math.round(project_todate['BAC']*100) / 100).toFixed(2);
+  totalbudget.textContent = '$ ' + parseFloat(project_todate['BAC']).toLocaleString('en');
 
   var bcwr = document.getElementById('bcwr');
-  bcwr.textContent = '$ ' + parseFloat(Math.round(project_todate['BCWR']*100) / 100).toFixed(2);
+  bcwr.textContent = '$ ' + parseFloat(project_todate['BCWR']).toLocaleString('en');
 
   var cpi = document.getElementById('cpi');
   cpi.textContent = parseFloat(Math.round(project_todate['CPI']*100)/100).toFixed(2);
 
   var cv = document.getElementById('cv');
-  cv.textContent = '$ ' + parseFloat(Math.round(project_todate['CV']*100) / 100).toFixed(2);
+  cv.textContent = '$ ' + parseFloat(project_todate['CV']).toLocaleString('en');
 
   var eac = document.getElementById('eac');
-  eac.textContent = '$ ' + parseFloat(Math.round(project_todate['EAC']*100) / 100).toFixed(2);
+  eac.textContent = '$ ' + parseFloat(project_todate['EAC']).toLocaleString('en');
 
   var percentcom = document.getElementById('percentcom');
   percentcom.textContent = parseFloat(Math.round(project_todate['PerComp'] *100) / 100).toFixed(4) * 100;
 
   var peac = document.getElementById('peac');
-  peac.textContent = '$ ' + parseFloat(Math.round(project_todate['PEAC']*100) / 100).toFixed(2);
+  peac.textContent = '$ ' + parseFloat(project_todate['PEAC']).toLocaleString('en');
 
   var petc = document.getElementById('petc');
-  petc.textContent = '$ ' + parseFloat(Math.round(project_todate['PETC']*100) / 100).toFixed(2);
+  petc.textContent = '$ ' + parseFloat(project_todate['PETC']).toLocaleString('en');
 
   var ptcpi = document.getElementById('ptcpi');
-  ptcpi.textContent = parseFloat(Math.round(project_todate['PTCPI']*100) / 100).toFixed(2);
+  ptcpi.textContent = parseFloat(project_todate['PTCPI']).toLocaleString('en');
 
   var spi = document.getElementById('spi');
-  spi.textContent = parseFloat(Math.round(project_todate['SPI']*100) / 100).toFixed(2);
+  spi.textContent = parseFloat(project_todate['SPI']).toLocaleString('en');
 
   var sv = document.getElementById('sv');
-  sv.textContent = '$ ' + parseFloat(Math.round(project_todate['SV']*100) /100).toFixed(2);
+  sv.textContent = '$ ' + parseFloat(project_todate['SV']).toLocaleString('en');
 
   var tcpi = document.getElementById('tcpi');
   tcpi.textContent = parseFloat(Math.round(project_todate['TCPI']*100) / 100).toFixed(2);
 
   var vac = document.getElementById('vac');
-  vac.textContent = '$' + parseFloat(Math.round(project_todate['VAC']*100) / 100).toFixed(2);
+  vac.textContent = '$' + parseFloat(project_todate['VAC']).toLocaleString('en');
 
   var etc = document.getElementById('etc');
-  etc.textContent = '$ ' + parseFloat(Math.round(project_todate['BCWR']*100) / 100).toFixed(2);
+  etc.textContent = '$ ' + parseFloat(project_todate['BCWR']).toLocaleString('en');
 
   var reportingpercent = document.getElementById('reportingpercent');
   reportingpercent.textContent = parseFloat(Math.round(project_todate['PerComp'] *100) / 100).toFixed(4) * 100 + '%';
